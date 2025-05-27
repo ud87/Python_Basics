3aab44ed0b9eea4213f350f7977937dbfe5b6106
unknown <udayarai@hotmail.co.uk>
2025-05-27 22:21:14 +0100

    Initial commit

---
+++ PythonFunctionApp/PythonFunction/PythonFunction.py
@@ -0,0 +1,9 @@
+#function is a piece of code that performs a specific task
+#function starts with def and can take parameter as input
+#python function name is written in snake case i.e. lower letters and _ is used to seperate words 
+
+def add_one(number):
+    return number + 1
+
+print("add_one(4): ")
+print( add_one(4))
