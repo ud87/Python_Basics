b0ddf7528489fb6275635aba9c5305ad31ae36c9
unknown <udayarai@hotmail.co.uk>
2025-05-29 20:41:28 +0100

    Function updated with excercise

--- PythonFunctionApp/PythonFunction/PythonFunction.py
+++ PythonFunctionApp/PythonFunction/PythonFunction.py
@@ -8,7 +8,33 @@ def add_one(number):
 print("add_one(4): ")
 print( add_one(4))
 
+
+#
+# @TASK: Write a function called `add_two` that:
+#
+# * Takes a number as input
+# * Adds two to it
+# * Returns the result
+
+# YOUR FUNCTION GOES BELOW THIS LINE
 def add_two(num):
     return num + 2
 
 print(add_two(2))
+
+
+# @TASK: Now you try. Here's an exercise for you:
+#
+# Write a function called `add_numbers` that:
+#
+# * Takes two numbers as input
+# * Adds them together
+# * Returns the result
+
+# YOUR FUNCTION GOES BELOW THIS LINE
+
+def add_numbers(num_1, num_2):
+    return num_1 + num_2
+
+print("add_numbers(1, 2) gives")
+print(add_numbers(1, 2))
