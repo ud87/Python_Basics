f534b868442e5279c1325350f58cfaecb69971c1
unknown <udayarai@hotmail.co.uk>
2025-06-06 14:34:15 +0100

    Initial commit

---
+++ PythonListsApp/PythonLists/PythonLists.py
@@ -0,0 +1,13 @@
+#lists
+student_names = ["Uday", "Makree", "Milan", "Tom"]
+print(student_names[-1])
+print(student_names[0])
+print(student_names[1:3])
+
+#add to list
+student_names.append("Harry")
+print(student_names)
+
+#to copy list
+my_copy = student_names.copy()
+print(my_copy)
